0d8008c07e1b4cbac7323c33be00c33f78c9b343
taharihrogers <45777031+taharihrogers@users.noreply.github.com>
2019-09-18 21:29:22 -0400

    Adding initial fan file

---
+++ Fan.java
@@ -0,0 +1,20 @@
+public class Fan {
+    int slow = 1;
+    int medium = 2;
+    int fast = 3;
+    private int speed = slow;
+    private boolean on = false;
+    String color = "blue";
+
+    //constructors, including a no arg one that creates a default fan
+
+    //accessor and mutator methods for all 4 fields
+
+    //toString
+    public String toString(){
+        //if fan is on, returns the fan speed, color, and radius in one combined string
+        //if fan is off, returns the fan color and radius along with the string "fan is off" in one combined string.
+        return null;
+    }
+
+}
