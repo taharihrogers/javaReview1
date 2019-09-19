afaf9d509362fe3ac030fbfb7ef42034c4210b73
taharihrogers <45777031+taharihrogers@users.noreply.github.com>
2019-09-18 21:46:34 -0400

    Updating Fan to add the constructors

--- Fan.java
+++ Fan.java
@@ -1,12 +1,35 @@
+
+/**
+ * Taharih Rogers
+ * Java Review 1
+ * Fan.java
+ * 9/18/2019
+ */
+
 public class Fan {
-    int slow = 1;
-    int medium = 2;
-    int fast = 3;
-    private int speed = slow;
-    private boolean on = false;
-    String color = "blue";
+    //variables
+    final int SLOW = 1;
+    final int MEDIUM = 2;
+    final int FAST = 3;
+    private int speed;
+    private boolean on;
+    private double radius;
+    String color;
 
     //constructors, including a no arg one that creates a default fan
+    public Fan(){
+        speed = SLOW;
+        on = false;
+        radius = 5;
+        color = "blue";
+    }
+
+    public Fan(int inSpeed, boolean inOn, double inRadius, String inColor){
+        speed = inSpeed;
+        on = inOn;
+        radius = inRadius;
+        color = inColor;
+    }
 
     //accessor and mutator methods for all 4 fields
 
